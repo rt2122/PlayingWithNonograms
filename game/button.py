9ee61176778260f15432de10cbff9cd7aa1ec536
rt2122 <alisa.nemeshaeva@gmail.com>
2022-04-12 15:48:19 +0300

    Add function for button.

--- game/button.py
+++ game/button.py
@@ -3,8 +3,9 @@ import pygame
 
 
 class MenuButton(pygame_gui.elements.UIButton):
-    def __init__(self, top, text, manager) -> None:
-        return super().__init__(relative_rect=pygame.Rect((350, top), (100, 50)),
+    def __init__(self, top, text, manager, next_page) -> None:
+        super().__init__(relative_rect=pygame.Rect((350, top), (100, 50)),
                                 text=text,
                                 manager=manager,
                                 object_id=pygame_gui.core.ObjectID(class_id='@menu_buttons'))
+        self.next_page = next_page
