602fd5f11c5e06ddef589cb55def934aa4abaf8d
rt2122 <alisa.nemeshaeva@gmail.com>
2022-06-09 15:56:34 +0300

    Index.html fixed & readthedocs without translation.

--- PlayingWithNonograms/tr.py
+++ PlayingWithNonograms/tr.py
@@ -7,6 +7,9 @@ lang = locale.getdefaultlocale()[0][:2]
 if lang != "ru":
     lang = "en"
 
-
-tr = gettext.translation("PWN", os.path.join(os.path.dirname(__file__), "./po"), [lang])
-_ = tr.gettext
+if not os.environ.get('READTHEDOCS'):
+    tr = gettext.translation("PWN", os.path.join(os.path.dirname(__file__), "./po"), [lang])
+    _ = tr.gettext
+else:
+    def _(x):
+        return x
