4e07d541d901b30689f246217bf3c56fc18443ae
Kate <flukatete@yandex.ru>
2022-06-13 13:04:29 +0300

    Modified for flake8.

--- PlayingWithNonograms/tests/test_progress_bar.py
+++ PlayingWithNonograms/tests/test_progress_bar.py
@@ -6,12 +6,13 @@ ws = (1920, 1080)
 pygame.init()
 manager = pygame_gui.UIManager(ws)
 
+
 def test_win():
-    pb = PlayingWithNonograms.progress_bar.GameProgressBar(pygame.Rect(0,0,0,0), manager)
+    pb = PlayingWithNonograms.progress_bar.GameProgressBar(pygame.Rect(0, 0, 0, 0), manager)
     pb.win = True
     assert pb.status_text()
 
 
 def test_lose():
-    pb = PlayingWithNonograms.progress_bar.GameProgressBar(pygame.Rect(0,0,0,0), manager)
+    pb = PlayingWithNonograms.progress_bar.GameProgressBar(pygame.Rect(0, 0, 0, 0), manager)
     assert not pb.status_text()
